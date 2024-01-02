dce0316acda5c97c655c69e2af35b4530bec45fc
Vishesh Panchal <panchavi@SYNOVERGETECH.COM>
2024-01-02 17:07:22 +0530

    4th commit

--- common/src/main/java/com/synoverge/common/utility/ErrorMessages.java
+++
@@ -1,9 +0,0 @@
-package com.synoverge.common.utility;
-
-public class ErrorMessages {
-
-    public static final String  CUSTOMER_NOT_FOUND = "Customer is not found!!...";
-
-
-
-}
--- common/src/main/java/com/synoverge/common/utility/SuccessMessage.java
+++
@@ -1,7 +0,0 @@
-package com.synoverge.common.utility;
-
-public class SuccessMessage {
-
-    public final static String REGISTER_SUCCESSFULLY = "Customer register successfully!!..";
-    public static final String  SUCCESS = "Fetch Successfully!!...";
-}
